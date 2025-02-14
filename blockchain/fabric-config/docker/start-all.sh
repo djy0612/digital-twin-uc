2ea0ce13ec027e1355ef22997a2b1070ae324697
djy <1983877110@qq.com>
2025-02-14 18:08:52 +0800

    添加了docker镜像文件

---
+++ blockchain/fabric-config/docker/start-all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 定义 docker-compose 文件路径
+DIR="$(dirname "$0")"  # 获取脚本所在目录
+COMPOSE_FILES=(
+    "$DIR/docker-compose-ca.yaml"
+    "$DIR/docker-compose-orderer.yaml"
+    "$DIR/docker-compose-org1.yaml"
+    "$DIR/docker-compose-org2.yaml"
+)
+
+echo "Starting all Docker Compose services..."
+
+for FILE in "${COMPOSE_FILES[@]}"; do
+    echo "Starting $FILE..."
+    docker-compose -f "$FILE" up -d
+done
+
+echo "All services started successfully!"
---
+++ blockchain/fabric-config/docker/stop-all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 定义 docker-compose 文件路径
+DIR="$(dirname "$0")"  # 获取脚本所在目录
+COMPOSE_FILES=(
+    "$DIR/docker-compose-ca.yaml"
+    "$DIR/docker-compose-org1.yaml"
+    "$DIR/docker-compose-org2.yaml"
+    "$DIR/docker-compose-orderer.yaml"
+)
+
+echo "Stopping all Docker Compose services..."
+
+for FILE in "${COMPOSE_FILES[@]}"; do
+    echo "Stopping $FILE..."
+    docker-compose -f "$FILE" down
+done
+
+echo "All services stopped successfully!"
